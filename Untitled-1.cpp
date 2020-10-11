3011b81baaf9bd811fa0accbe02bedcdd809a8ed
Lsovo <787243066@qq.com>
2020-10-11 18:20:43 +0800

    tt

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
-    int a,b,sum;
-    scanf("%d%d",&a,&b);
-    sum=a+b;
-    printf("a+b=%d",sum);
+    int a,b,c,sum;
+    scanf("%d%d",&a,&b,&c);
+    sum=a+b+c;
+    printf("a+b+c=%d",sum);
 }
